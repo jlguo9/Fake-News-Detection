77dec361981138ef00749544875bc6205b486c9c
Jialiang Guo <jialiang_guo@sfu.ca>
2023-04-12 17:05:16 -0700

    adjust probability according to source credibility

--- Project/web_apps/backend-app/server.py
+++ Project/web_apps/backend-app/server.py
@@ -5,29 +5,62 @@ from project733 import *
 
 app = Flask(__name__)
 m1 = None
-m2= None
+m2 = None
+m3 = None
 CORS(app)
 
 
+credibility= {
+    'nytimes.com': 0.02,
+    'washingtonpost.com': 0.02,
+    'wsj.com': 0.02,
+    'apnews.com': 0.02,
+    'bloomberg.com': 0.02,
+    'bcc.com': 0.02,
+    'theguardian.com': 0.02,
+    'reuters.com': 0.02,
+    'cctv.com': -0.05,
+    'xinhuanet.com': -0.05,
+    'dailymail.co.uk': -0.02,
+    'huffpost.com': -0.02,
+    'forbes.com': -0.05,
+    'gizmodo.com': -0.02,
+    'qsheory.com': -0.05,
+    'infowars.com': -0.1,
+    'dailystar.co.uk': -0.1,
+    'nationalenquirer.com': -0.1,
+    'breitbart.com': -0.1,
+    'beforeitsnews.com': -0.1,
+    'globalresearch.ca': -0.1,
+    'yournewswire.com': -0.1,
+    'worldtruth.tv': -0.1,
+    'zerohedge.com': -0.1,
+}
+
 def run_model(newsObj):
-    r1 = m1(newsObj['description'])
-    r2 = m2(newsObj['description'])
-    r3 = m3(newsObj['description'])
+    source = newsObj['source']
+    r1 = m1(newsObj['description'])[1]
+    r2 = m2(newsObj['description'])[1]
+    r3 = m3(newsObj['description'])[1]
+    if source in credibility:
+        r1 -= credibility[source]
+        r2 -= credibility[source]
+        r3 -= credibility[source]
     return jsonify([
         {
         "modelId": 1,
         "model":"Bert",
-        "fake": r1[1]
+        "fake": r1
         },
         {
         "modelId": 2,
         "model":"GPT2",
-        "fake": r2[1]
+        "fake": r2
         },
         {
         "modelId": 3,
         "model":"LSTM",
-        "fake": r3[1]
+        "fake": r3
         }
     ])
     
